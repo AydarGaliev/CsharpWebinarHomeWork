d70545b88211c933757b69f8df9da54b48061438
Aydar <3ajimka@gmail.com>
2022-04-18 22:44:44 +0300

    Второй вебинар: третья задача  вывод кратности с учётом остатка

---
+++ SecondWebinar/3Task/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if(number2 % number1 != 0)
+{
+    Console.Write("Число не кратно остаток: ");
+    Console.WriteLine(Division(number2, number1));
+}
+else
+{
+    Console.WriteLine("Число кратно");
+}
+
+
+int Division(int number2, int number1)
+{
+    return number2 % number1;
+}
