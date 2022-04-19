68b5a74a1a7093b25ef2b689b0890086bf2b0496
Aydar <3ajimka@gmail.com>
2022-04-19 18:40:23 +0300

    решение третьей задачи с днями недели

---
+++ SecondWebinar/6Task/Program.cs
@@ -0,0 +1,28 @@
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+//6 -> да
+//7 -> да
+//1 -> нет
+
+Console.WriteLine("Является ли день выходным?");
+Console.WriteLine("Введите число от 1 до 7: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Days(number);
+
+void Days(int numday)
+{
+    if(numday > 7)
+    {
+        Console.WriteLine("В неделе 7 дней");
+    }
+    else
+    {
+        if(numday == 6 || numday == 7)
+        {
+            Console.WriteLine("Да");
+        }
+        else
+        {
+            Console.WriteLine("Нет");
+        }
+    }
+}
