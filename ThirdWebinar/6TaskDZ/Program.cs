39eafc8ad14a259e4c5ff193dceaf6098365e801
Aydar <3ajimka@gmail.com>
2022-04-21 02:25:04 +0300

    Домашнее задание на расстояние xyz

---
+++ ThirdWebinar/6TaskDZ/Program.cs
@@ -0,0 +1,24 @@
+﻿double x1 = Coordinate("x", "1");
+double y1 = Coordinate("y", "1");
+double z1 = Coordinate("z", "1");
+double x2 = Coordinate("x", "2");
+double y2 = Coordinate("y", "2");
+double z2 = Coordinate("z", "2");
+
+int Coordinate(string coorName, string pointName)
+{
+    Console.Write($"Введите координату {coorName} точки {pointName}: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+double a = Convert.ToDouble(Math.Pow((x1 - x2), 2));
+double b = Convert.ToDouble(Math.Pow((y1 - y2), 2));
+double c = Convert.ToDouble(Math.Pow((z1 - z2), 2));
+double d = a + b + c;
+double distance = Gipotenusa(d);
+
+double Gipotenusa(double sum)
+{
+    return Math.Sqrt(sum);
+}
+Console.WriteLine($"Расстояние между точками: {distance}");
