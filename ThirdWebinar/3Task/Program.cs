7395370b5baf1b1f87621f44a6dd86685e38dd1f
Aydar <3ajimka@gmail.com>
2022-04-21 00:12:30 +0300

    Задача на рассчёт расстояние между точками

---
+++ ThirdWebinar/3Task/Program.cs
@@ -0,0 +1,24 @@
+﻿//Строки  2-12 функция ввода.
+double x1 = Coordinate("x", "1");
+double y1 = Coordinate("y", "1");
+double x2 = Coordinate("x", "2");
+double y2 = Coordinate("y", "2");
+
+
+int Coordinate(string coorName, string pointName)
+{
+    Console.Write($"Введите координату {coorName} точки {pointName}: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+double x = (x1 - x2)*(x1 - x2);     // Math.Pow - выведение квадрата
+double y = (y1 - y2)*(y1 - y2);
+double d = x + y;
+double distance = Gipotenusa(d);
+
+double Gipotenusa(double sum)
+{
+    return Math.Sqrt(sum);
+}
+Console.WriteLine($"Расстояние между точками: {distance}");
