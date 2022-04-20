75f5e09681802588096b40226d8af32eeae95fc8
Aydar <3ajimka@gmail.com>
2022-04-20 23:36:25 +0300

    Корректировка орфографических ошибок!

--- ThirdWebinar/2Task/Program.cs
+++ ThirdWebinar/2Task/Program.cs
@@ -1,9 +1,9 @@
-﻿Console.Write("Введите номер плоскости координат: ");
+﻿Console.Write("Введите номер четверти плоскостей координат: ");
 int quarter = Convert.ToInt32(Console.ReadLine());
 
-if(quarter == 0)
+if(quarter == 0 || quarter > 4)
 {
-    Console.WriteLine("Координат в данной плоскости не существует.");
+    Console.WriteLine("Введите корректный номер четверти плоскостей.");
 }
 
 if(quarter == 1)
@@ -12,13 +12,13 @@ if(quarter == 1)
 }
 if(quarter == 2)
 {
-    Console.WriteLine($"Диапозон координат в плоскости {quarter} по X от от - бесконечности до 0 и по Y от 0 до бесконечности");
+    Console.WriteLine($"Диапозон координат в плоскости {quarter} по X от - бесконечности до 0 и по Y от 0 до бесконечности");
 }
 if(quarter == 3)
 {
-    Console.WriteLine($"Диапозон координат в плоскости {quarter} по X от от - бесконечности до 0 и по Y от - бесконечности до 0");
+    Console.WriteLine($"Диапозон координат в плоскости {quarter} по X от - бесконечности до 0 и по Y от - бесконечности до 0");
 }
-if(quarter == 3)
+if(quarter == 4)
 {
     Console.WriteLine($"Диапозон координат в плоскости {quarter} по X от 0 до бесконечности и по Y от - бесконечности до 0");
 }
