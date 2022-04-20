b314bf9022734819fdbc6e4e640243ab5c4c16e5
Aydar <3ajimka@gmail.com>
2022-04-21 01:45:39 +0300

    Домашнее задание на палиандром

---
+++ ThirdWebinar/5TaskDZ/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Проверка числа на палиандром");
+Console.Write("Введите пятизначное число N: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Paliandrom(a);
+
+int Paliandrom(int x)
+{
+    if((x / 10000 == x % 10) && (((x / 1000) % 10)) == ((x % 100) / 10))
+    {
+        Console.WriteLine("Число является палиандромом");
+        return x;
+    }
+    else
+    {
+        Console.WriteLine("Число не является палиандромом");
+        return x;
+    }
+}
