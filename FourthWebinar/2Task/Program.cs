af5162419b49c6d549558678bcd1c0286bfac8c8
Aydar <3ajimka@gmail.com>
2022-04-24 19:58:36 +0300

    3 способ решения задачи

--- FourthWebinar/2Task/Program.cs
+++ FourthWebinar/2Task/Program.cs
@@ -1,14 +1,24 @@
-﻿/* Console.WriteLine("Введите число: ");
+﻿/* Console.WriteLine("Введите число: ");    // 1 способ
 string number = Console.ReadLine();
 int quantity = number.Length;
 Console.WriteLine(quantity); */
 
 
-Console.WriteLine("Введите число: ");
+/* Console.WriteLine("Введите число: ");    // 2 способ через цикл
 string number = Console.ReadLine();
 int result = 0;
 for(int i = 0; i < number.Length; i++)
 {
     result++;
 }
-Console.WriteLine(result);
+Console.WriteLine(result); */
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int index = 0;
+while(number != 0)
+{
+    number = number / 10;
+    index++;
+}
+Console.WriteLine(index);
