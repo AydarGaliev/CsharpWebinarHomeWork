1f2e5e4f3c57ace4cbe72e3111655f8f6418191b
Aydar <3ajimka@gmail.com>
2022-04-24 22:03:55 +0300

    Домашние задание суммирование цифр в числе

---
+++ FourthWebinar/6TaskDZ/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int index = 0;
+int sum = 0;
+while(number != 0)
+{
+    sum = sum + (number % 10);
+    index++;
+    number = number / 10;
+}
+Console.WriteLine(sum);
