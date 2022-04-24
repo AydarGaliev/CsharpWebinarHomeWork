8f325d8886de6802154468d1cb4e2dd8cc312528
Aydar <3ajimka@gmail.com>
2022-04-24 21:56:03 +0300

    Домашнее задание на возведение числа А в степени В

---
+++ FourthWebinar/5TaskDZ/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+for(int i = 0; i < numberB; i++)
+{
+    numberA = numberA * numberA;
+    i++;
+}
+Console.WriteLine("Ответ: " + numberA);
