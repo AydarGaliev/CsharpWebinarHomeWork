2a1e697044e46f0f9bf1b7c0afcfed6610f220b5
Aydar <3ajimka@gmail.com>
2022-04-24 19:35:23 +0300

    Задача на вывод суммы числе от 1 до n

---
+++ FourthWebinar/1Task/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+for(int i = 1; i <= number; i++)
+{
+    sum += i;
+}
+Console.WriteLine("Ответ: " + sum);
