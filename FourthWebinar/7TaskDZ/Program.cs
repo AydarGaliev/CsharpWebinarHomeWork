77b47d3da6769802ee5b3044c29179cb9f81b8f4
Aydar <3ajimka@gmail.com>
2022-04-24 22:24:54 +0300

    Домашнее задание на заполнение массива и его вывод

---
+++ FourthWebinar/7TaskDZ/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Тут будет массив");
+int[] array = new int[8];
+void FillArray(int[] arr)
+{
+    for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(0, 20);
+    }
+    
+}
+void PrintAray(int[] arrayInternal)
+{ 
+    int count = arrayInternal.Length;
+    Console.Write("{");
+    for(int i = 0; i < count - 1; i++)
+    {
+        Console.Write($"{arrayInternal[i]}, ");
+    }
+    Console.WriteLine(arrayInternal[count - 1] + "}");
+}
+FillArray(array);
+Console.Write("Исходный массив: ");
+PrintAray(array);
