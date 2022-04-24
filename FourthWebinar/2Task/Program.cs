c8580a5da252e955607bd39aec4dc21983154cc9
Aydar <3ajimka@gmail.com>
2022-04-24 19:48:23 +0300

    Задача на вывод количества цифр в числе

---
+++ FourthWebinar/2Task/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+int quantity = number.Length;
+Console.WriteLine(quantity); */
+
+
+Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+int result = 0;
+for(int i = 0; i < number.Length; i++)
+{
+    result++;
+}
+Console.WriteLine(result);
