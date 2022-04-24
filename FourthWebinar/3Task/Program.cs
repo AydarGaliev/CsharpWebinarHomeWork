48903792e9648ec3c07a305457ceb5ad97584dbd
Aydar <3ajimka@gmail.com>
2022-04-24 21:07:20 +0300

    Задача на произведение цифр числа

---
+++ FourthWebinar/3Task/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число: ");
+int factorial = Convert.ToInt32(Console.ReadLine());
+int a = 1;
+for(int i = 1; i <= factorial; i++)
+{
+    a = a * i;
+}
+Console.WriteLine(a);
