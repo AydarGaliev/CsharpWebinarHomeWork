7906dd57fd7f5021023a088a03b914aa1e7a0529
Aydar <3ajimka@gmail.com>
2022-04-25 22:06:59 +0300

    4-5 задачи

---
+++ FifthWebinar/4Task/Program.cs
@@ -0,0 +1,36 @@
+﻿//Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+//Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+//[5, 18, 123, 6, 2] -> 1
+//[1, 2, 3, 6, 2] -> 0
+//[10, 11, 12, 13, 14] -> 5
+
+int[] numbers = new int[123];
+FillArray(numbers);
+WriteArray(numbers);
+int a = 0;
+
+for(int i = 0; i < numbers.Length; i++)
+{
+    if(numbers[i] >= 10 && numbers[i] <= 99)
+    {
+        a++;
+    }
+}
+Console.Write($"Количество элементов интервале от 10 до 99: {a}");
+
+void FillArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-99, 200);
+    }
+}
+
+void WriteArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
---
+++ FifthWebinar/5Task/Program.cs
@@ -0,0 +1,47 @@
+﻿// Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+//[1 2 3 4 5] -> 5 8 3
+//[6 7 3 6] -> 36 21
+
+Console.WriteLine("Введите длину массива ");
+int Length = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[Length];
+FillArray(numbers);
+WriteArray(numbers);
+int Lengthsum;
+if(Length % 2 == 0)
+{
+    Lengthsum = Length / 2;
+}
+else
+{
+    Lengthsum = (Length / 2) + 1;
+}
+
+int[] sumarr = new int[Lengthsum];
+
+for(int i = 0; i < sumarr.Length; i++)
+{
+    sumarr[i] = numbers[i] * numbers[numbers.Length-1-i];
+}
+if(Length % 2 != 0)
+{
+    sumarr[sumarr.Length-1] = numbers[sumarr.Length-1];
+}
+WriteArray(sumarr);
+
+void FillArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-20, 20);
+    }
+}
+
+void WriteArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
