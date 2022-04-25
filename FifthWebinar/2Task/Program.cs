5d7e8850e0db61ccede5b28acb8821a957ffe794
Aydar <3ajimka@gmail.com>
2022-04-25 21:00:01 +0300

    Задачи 1-3

---
+++ FifthWebinar/2Task/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 1.Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+
+// Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+
+// 1 Задача
+
+int[] numbers = new int[6];
+FillArray(numbers);
+WriteArray(numbers);
+
+for(int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = -numbers[i];
+    Console.Write(numbers[i] + " ");
+}
+
+void FillArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write("Введите элемент № " + (i + 1) + ": ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+void WriteArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
---
+++ FifthWebinar/3Task/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+
+int[] numbers = new int[6];
+FillArray(numbers);
+WriteArray(numbers);
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+for(int i = 0; i < numbers.Length; i++)
+{
+    if(a == numbers[i])
+    {
+        Console.Write($"Число {a} присутствует в массиве.");
+        return;
+    }
+}
+Console.Write($"Число {a} отсутствует в массиве.");
+
+void FillArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write("Введите элемент № " + (i + 1) + ": ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+void WriteArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
