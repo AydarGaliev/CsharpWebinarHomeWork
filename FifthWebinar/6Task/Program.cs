189ca0ea461178cf812c40efcafb439da35de157
Aydar <3ajimka@gmail.com>
2022-04-28 20:26:37 +0300

    Задача на подсчёт чётных чисел в массиве

---
+++ FifthWebinar/6Task/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Введите длину массива ");
+int Length = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[Length];
+int count = 0;
+FillArray(numbers);
+WriteArray(numbers);
+
+for(int i = 0; i < numbers.Length; i++)
+{
+    if(numbers[i] % 2 == 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine("Количество чётных чисел: " + count);
+
+void FillArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 999);
+    }
+}
+
+void WriteArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
