5616629f3cbe4c061d44fb03609019c64f8787b0
Aydar <3ajimka@gmail.com>
2022-04-28 21:15:55 +0300

    Задача на нахождение разницы между максимум и минимум массива

---
+++ FifthWebinar/8TaskDZ/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("Введите длину массива ");
+int Length = Convert.ToInt32(Console.ReadLine());
+double[] numbers = new double[Length];
+double min = numbers[0];
+double max = numbers[0];
+double dif = 0;
+FillArray(numbers);
+WriteArray(numbers);
+
+for(int i = 0; i < numbers.Length; i++)
+{
+    if(numbers[i] > max)
+    {
+        max = numbers[i];
+    }
+    if(numbers[i] < min)
+    {
+         min = numbers[i];
+    }
+    dif = max - min;
+}
+Console.WriteLine("Разница между max и min числами массива: " + dif);
+
+void FillArray(double[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-99, 99);
+    }
+}
+
+void WriteArray(double[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
