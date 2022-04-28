254e8753d2d28b0e0a18914789ecfe42608de886
Aydar <3ajimka@gmail.com>
2022-04-28 20:38:39 +0300

    Задача на сложение чисел массива с нечётным индексом

---
+++ FifthWebinar/7TaskDZ/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Введите длину массива ");
+int Length = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[Length];
+int count = 0;
+int sum = 0;
+FillArray(numbers);
+WriteArray(numbers);
+
+for(int i = 1; i < numbers.Length; i++)
+{
+    count = numbers[i] + numbers[i + 2];
+    i = i + 2;
+    sum = count;
+}
+Console.WriteLine(sum);
+
+void FillArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-99, 99);
+    }
+}
+
+void WriteArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
