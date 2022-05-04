0ae2e20af1fd002db09feab9080598166161684c
Aydar <3ajimka@gmail.com>
2022-05-04 21:53:29 +0300

    Решение задачи перевод числа в двоичную систему

---
+++ SixthWebinar/3Task/Program.cs
@@ -0,0 +1,16 @@
+﻿ int InputNumber(string chislo)
+{
+    Console.WriteLine($"Введите число {chislo}");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+ int a = InputNumber("А");
+ string result = "" + a % 2;
+
+ while(a >= 2)
+{
+    a = a / 2;
+    int c = a % 2;
+    result = c + result;
+}
+Console.Write(result);
