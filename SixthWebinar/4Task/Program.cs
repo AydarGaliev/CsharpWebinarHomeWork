e58c112f8697798b761f7fd36be0d25044c79b32
Aydar <3ajimka@gmail.com>
2022-05-04 22:03:23 +0300

    Решение задачи вывод n чисел Фибоначи

---
+++ SixthWebinar/4Task/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите длинну массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] Fibonacci = new int [length];
+Fibonacci[0] = 0;
+Fibonacci[1] = 1;
+for(int i = 2; i < length; i++)
+{
+    Fibonacci[i] = Fibonacci[i - 1] + Fibonacci[i - 2];
+}
+WriteArray(Fibonacci);
+
+void WriteArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
