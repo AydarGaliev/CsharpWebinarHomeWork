8958b69298ee98574c40faa1c33ca1ea211ff9cf
Aydar <3ajimka@gmail.com>
2022-05-04 20:35:08 +0300

    Решение задачи могут ли введенные числа быть треуголником

--- SixthWebinar/2Task/Program.cs
+++ SixthWebinar/2Task/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿int InputNumber(string name)
+{
+    Console.WriteLine($"Введите число {name} ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+int a = InputNumber("A");
+int b = InputNumber("B");
+int c = InputNumber("C");
+
+if(a < b + c && b < a + c && c < a + b)
+{
+    Console.WriteLine("Это треуголник");
+}
+else 
+{
+    Console.WriteLine("Это не треуголник");
+}
