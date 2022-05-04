7e087c02f35e784d13c68d588b54d969ee6c7e26
Aydar <3ajimka@gmail.com>
2022-05-04 22:24:43 +0300

    Домашнее задание на вывод количества положительных чисел массива

---
+++ SixthWebinar/6TaskDZ/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите длину массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[length];
+
+for (int i = 0; i < length; i++)
+{
+    Console.WriteLine($"Введите {i + 1} элемент массива");
+    array[i] = Convert.ToDouble(Console.ReadLine());
+}
+int count = 0;
+for(int i = 0; i<length;i++)
+{
+    if(array[i]>0)
+    {
+        count++; 
+    }
+    else
+    {
+        count = count + 0;
+    }    
+}
+Console.WriteLine($"Количество положительных чисел в массиве = {count} ");
