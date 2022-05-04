c2149b617939525c4534ca996d660f0e515c0acb
Aydar <3ajimka@gmail.com>
2022-05-04 22:17:54 +0300

    Решение задачи создание копии массива поэлементным копированием

---
+++ SixthWebinar/5Task/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Введите длинну массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] Fibonacci = new int [length];
+
+FillArray(Fibonacci);
+WriteArray(Fibonacci);
+
+int[] newArray = new int [length];
+for(int i = 0; i < length; i++)
+{
+    newArray[i] = Fibonacci[i];
+}
+Console.Write("Копия массива: ");
+WriteArray(newArray);
+
+void FillArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-9, 10);
+    }
+}
+
+void WriteArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
