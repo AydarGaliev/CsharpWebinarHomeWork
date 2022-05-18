f553aff4a77b5a1625abf7a1bc248ec6d5e6ffe5
Aydar <3ajimka@gmail.com>
2022-05-18 22:13:28 +0300

    Задача на рандомное заполнение двумерного массива

---
+++ SeventhWebinar/1Task/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[m, n];
+
+for(int i = 0; i < m; i++)
+{
+    for(int j = 0; j < n; j++)
+    {
+        array[i, j] = new Random().Next(-10, 101);
+    }
+}
+
+for(int i = 0; i < m; i++)
+{
+    for(int j = 0; j < n; j++)
+    {
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+}
