cdd47b4cce77ebaa38a372c08af8d672d384d6ae
Aydar <3ajimka@gmail.com>
2022-05-19 01:14:55 +0300

    Задача на нахождение среднего арифметического столбцов массива

---
+++ SeventhWebinar/7TaskDZ/Program.cs
@@ -0,0 +1,45 @@
+﻿Console.Write("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[m, n];
+
+CreateArray(array);
+WriteArray(array);
+Console.Write("Среднее арифметическое столбцов: ");
+
+for (int j = 0; j < n; j++)
+{
+    double sum = 0;
+    double average = 0;
+    for (int i = 0; i < m; i++)
+    {
+        sum += array[i, j];
+    }
+    average = sum / m;
+    Console.Write(Math.Round(average,1) + "; ");
+}
+
+void CreateArray(int[,] array)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void WriteArray (int[,] array)
+{
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
