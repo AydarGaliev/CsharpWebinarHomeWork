e754b4574b0403dde7a8d653a4e0b46419341de0
Aydar <3ajimka@gmail.com>
2022-05-23 09:34:49 +0300

    Решение задачи на вывод трехмерного массива с индексом элементов

---
+++ EighthWebinar/8TaskDZ/Program.cs
@@ -0,0 +1,26 @@
+﻿int[,,] array3D = new int[3,3,3];
+int count = 10;
+
+
+for (int i = 0; i < array3D.GetLength(0); i++)
+{
+    for (int j = 0; j < array3D.GetLength(1); j++)
+    {
+        for (int k = 0; k < array3D.GetLength(2); k++)
+        {
+            array3D[i,j,k] = count;
+            count++;
+        }
+    }
+}
+
+for (int i = 0; i <array3D.GetLength(0); i++)
+{
+    for (int j = 0; j < array3D.GetLength(1); j++)
+    {
+        for (int k = 0; k < array3D.GetLength(2); k++)
+        {
+            Console.WriteLine(array3D[i,j,k] + $" - Строка: {i + 1}; Столбец: {j + 1}; Толщина: {k + 1}");
+        }
+    }
+}
