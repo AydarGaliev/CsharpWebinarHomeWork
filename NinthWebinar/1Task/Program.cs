f0b32044d1c3e736b08cfeaf2d55f5a7d4e3808b
Aydar <3ajimka@gmail.com>
2022-05-26 00:08:27 +0300

    Решение задачи на вывод натуральных чисел до введенного числа N

---
+++ NinthWebinar/1Task/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int result = Rekurs(n);
+Console.WriteLine(result);
+
+int Rekurs(int number)
+{
+    if(number == 1)
+        return 1;
+    int temp = Rekurs(number - 1);
+    Console.Write(temp + ", ");
+    return number;
+}
