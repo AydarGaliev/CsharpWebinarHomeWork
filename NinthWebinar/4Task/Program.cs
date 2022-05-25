46e7f4876c0fc47a6176ea1f7ba1d2b15a9f9965
Aydar <3ajimka@gmail.com>
2022-05-26 00:52:54 +0300

    Решение задачи на вывод введенного числа в введенной степени

---
+++ NinthWebinar/4Task/Program.cs
@@ -0,0 +1,19 @@
+﻿int number = Input("Введите число: ");
+int stepen = Input("Введите степень: ");
+
+Console.WriteLine(OutPutFunction(number, stepen));
+
+int Input(String text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int OutPutFunction(int numStart, int numEnd)
+{
+    if(numEnd == 0)
+    {
+        return 1;
+    }
+    return numStart * OutPutFunction(numStart, numEnd - 1);
+}
