711aad11c21cadd325986708abf98780f46f01ed
Aydar <3ajimka@gmail.com>
2022-05-26 01:02:52 +0300

    Решение задачи на нахождение суммы натуральных элементов в вводимом промежутке

---
+++ NinthWebinar/6TaskDZ/Program.cs
@@ -0,0 +1,21 @@
+﻿int mNumber = Input("Введите значение M: ");
+int nNumber = Input("Введите значение N: ");
+
+int result = RekursSum(nNumber, mNumber);
+Console.WriteLine(result + " ");
+
+int RekursSum(int nNumber, int mNumber)
+{  
+    if (nNumber <= mNumber) 
+    {
+    return nNumber;
+    }
+    
+    return nNumber + RekursSum(nNumber-1,mNumber);
+}
+
+int Input(String text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
