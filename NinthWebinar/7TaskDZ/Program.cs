0b54990e1a0210ac10a0559af71c4889c58483da
Aydar <3ajimka@gmail.com>
2022-05-26 01:08:25 +0300

    Решение задачи на функцию Аккермана

---
+++ NinthWebinar/7TaskDZ/Program.cs
@@ -0,0 +1,30 @@
+﻿int mNumber = Input("Введите значение M: ");
+int nNumber = Input("Введите значение N: ");
+
+int result = Rekursion(nNumber,mNumber);
+Console.WriteLine(result);
+
+int Rekursion(int nNumber, int mNumber)
+{
+    if (nNumber == 0)
+    {
+        return mNumber + 1;
+    }
+    else
+    {
+        if ((nNumber != 0) && (mNumber == 0))
+        {
+            return Rekursion(nNumber - 1, 1);
+        }
+        else
+        {
+            return Rekursion(nNumber - 1,Rekursion(nNumber, mNumber - 1));
+        }
+    }
+}
+
+int Input(String text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
