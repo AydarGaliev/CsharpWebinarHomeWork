57e658891ff182b9214c6b544c5dbea82897ad7c
Aydar <3ajimka@gmail.com>
2022-05-26 00:23:53 +0300

    Решение задачи на вывод натуральных чисел от N до M

---
+++ NinthWebinar/2Task/Program.cs
@@ -0,0 +1,20 @@
+﻿int numberStart = Input("Введите значение M: ");
+int numberEnd = Input("Введите значение N: ");
+
+Console.WriteLine(OutPutFunction(numberStart, numberEnd));
+
+int Input(String text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int OutPutFunction(int numStart, int numEnd)
+{
+    if(numEnd == numStart)
+    {
+        return numEnd;
+    }
+    Console.Write(OutPutFunction(numStart, numEnd - 1) + ", ");
+    return numEnd;
+}
