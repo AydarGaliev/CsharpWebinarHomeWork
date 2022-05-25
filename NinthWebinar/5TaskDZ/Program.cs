c9cbf04fe8d35e325143380e3490a5e1c479eba8
Aydar <3ajimka@gmail.com>
2022-05-26 00:59:35 +0300

    Решение задачи на вывод натуральных чисел от N до 1

---
+++ NinthWebinar/5TaskDZ/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Задайте число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int decreasing = 1;
+int result = Rekurs(number, decreasing);
+Console.WriteLine(result);
+
+int Rekurs(int number, int decreasing)
+{  
+    if (decreasing == number) 
+    {
+    return decreasing;
+    }
+    Console.Write(Rekurs(number, decreasing + 1) + ", ");
+    return decreasing;
+}
