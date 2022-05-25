a6d4661ae72f4b7c931f1caac72902338902cdc9
Aydar <3ajimka@gmail.com>
2022-05-26 00:43:50 +0300

    Решение задачи на вывод суммы цифр вводимого числа

---
+++ NinthWebinar/3Task/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = Summ(number);
+Console.WriteLine(result);
+
+int Summ(int number)
+{   
+    if(number > 0)
+    {
+        return number%10 + Summ(number/10);
+    }
+    return 0;
+}
