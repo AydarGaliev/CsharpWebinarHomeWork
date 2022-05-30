950fb417d91933c753ccfd39d055c12a1556cc1f
Aydar <3ajimka@gmail.com>
2022-05-30 18:30:10 +0300

    Решение задачи на генерацию слов из введенных букв с вводом длины слов

---
+++ TenthWebinar/2Task/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите буквы: ");
+string charWords = Console.ReadLine();
+
+Console.WriteLine("Введите длину слов: ");
+int n = int.Parse(Console.ReadLine());
+PrintAllWorlds(charWords, n, "");
+
+void PrintAllWorlds(string alphabet, int length, string prefix)
+{
+    if(length == 0)
+        Console.WriteLine(prefix);
+    else
+    {
+        foreach(var c in alphabet)
+        {
+            PrintAllWorlds(alphabet, length - 1, prefix + c);
+        }
+    }
+}
