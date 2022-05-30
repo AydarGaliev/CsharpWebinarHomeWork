8bf1468336e82c6e3444b3bd40baea263b541d31
Aydar <3ajimka@gmail.com>
2022-05-30 18:28:43 +0300

    Решение задачи функции Фибоначчи с вводом чисел и длины функции

---
+++ TenthWebinar/1Task/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите первое число");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Сколько чисел вывести?");
+int s = Convert.ToInt32(Console.ReadLine());
+
+FibonacciModified(n, m, s);
+
+void FibonacciModified(int n, int m, int s)
+{
+    Console.Write(n + " ");
+    Console.Write(m + " ");
+    int sum = 0;
+    for (int i = 2; i < s; i++)
+    {
+        sum = n + m;
+        Console.Write(sum + " ");
+        {
+            n = m;
+            m = sum;
+        }
+    }
+}
